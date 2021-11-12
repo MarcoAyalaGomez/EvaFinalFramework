26aed7e75c0be418693cf11bc682869db80e22aa
felipe <felipe.sanmartin.valenzuela@ciisa.cl>
2021-11-11 23:29:06 -0300

    commit inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master') 
+@section ('title', 'home')
+@section('header')
+@section('content')
+
+<div class="container w-75 bg-info mt-5 rounded shadow">
+        <div class="row align-items-stretch">
+            
+        <div class="container w-75 bg2 mt-5 rounded shadow ">
+        <div class="text-center">
+            <hr>
+        <h3>Bienvenid@ administrador, aquí puedes gestionar los productos que desees</h3>
+        <h1 class="display">Visitanos en YouTube para más contenido</h1>
+        <p class="lead"><a class="btn btn-outline-danger" href="https://www.youtube.com/watch?v=hiT7Q1wLfgA">Link aquí</a></p>
+        <hr>
+
+        
+    </div>
+    </div>
+            <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded" ></div>
+        </div>
+    </div>
+
+
+@stop
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController; 
+
+
+
+Route::get('/login',function(){
+    return view ('login');
+})->name('login')->middleware('guest');
+
+Route::get('/home',function(){
+    return view('welcome');
+})->middleware('auth')->name('home');
+
+Route::post('/login',[LoginController::class,'login']);
+Route::put('/login',[LoginController::class,'logout']); 
+
+
+Route::resource('productos','App\Http\Controllers\ProductoController');
